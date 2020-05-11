5186467772ec8598be01c0b12de6cc046eb29314
parth sarthi prasad <mepsp2000@gmail.com>
2020-05-12 04:59:37 +0530

    fixed textoverlay

--- linux/simple/ip_simulation.py
+++ linux/simple/ip_simulation.py
@@ -31,7 +31,7 @@ class SensorFactory(GstRtspServer.RTSPMediaFactory):
         self.duration = 1 / self.fps * Gst.SECOND  # duration of a frame in nanoseconds
         self.launch_string = 'appsrc name=source is-live=true block=true format=GST_FORMAT_TIME ' \
                              'caps=video/x-raw,format=BGR,width=1280,height=720,framerate={}/1 ' \
-                             '! textoverlay text="E2E VIZION" valignment=top halignment=left font-desc="Sans, 20" ' \
+                             '! textoverlay text="NOIZIV" valignment=top halignment=left font-desc="Sans, 20" ' \
                              '! videoconvert ! video/x-raw,format=I420 ' \
                              '! x264enc speed-preset=ultrafast tune=zerolatency ' \
                              '! rtph264pay config-interval=1 name=pay0 pt=96'.format(self.fps)
