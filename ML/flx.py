d1635c9391772a53b47233ec131b315c907dcd42
24Beast <40471796+24Beast@users.noreply.github.com>
2020-05-24 17:59:59 +0530

    Update

--- ML/flx.py
+++ ML/flx.py
@@ -1,10 +1,116 @@
 import tensorflow as tf
 import numpy as np
-import transformation
 
 img_crop = 3
 
 #Figure out the tf concrete function problem
+
+def repeat(x, num_repeats):
+    with tf.name_scope("repeat"):
+        ones = tf.ones((1, num_repeats), dtype='int32')
+        x = tf.reshape(x, shape=(-1,1))
+        x = tf.matmul(x, ones)
+        return tf.reshape(x, [-1])
+
+def interpolate(image, x, y, output_size):
+    with tf.name_scope("interpolate"):
+        batch_size = tf.shape(image)[0]
+        height = tf.shape(image)[1]
+        width = tf.shape(image)[2]
+        num_channels = tf.shape(image)[3]
+
+        x = tf.cast(x , dtype='float32')
+        y = tf.cast(y , dtype='float32')
+
+        height_float = tf.cast(height, dtype='float32')
+        width_float = tf.cast(width, dtype='float32')
+
+        output_height = output_size[0]
+        output_width  = output_size[1]
+
+        x = .5*(x + 1.0)*(width_float)
+        y = .5*(y + 1.0)*(height_float)
+
+        x0 = tf.cast(tf.floor(x), 'int32')
+        x1 = x0 + 1
+        y0 = tf.cast(tf.floor(y), 'int32')
+        y1 = y0 + 1
+
+        max_y = tf.cast(height - 1, dtype='int32')
+        max_x = tf.cast(width - 1,  dtype='int32')
+        zero = tf.zeros([], dtype='int32')
+
+        x0 = tf.clip_by_value(x0, zero, max_x)
+        x1 = tf.clip_by_value(x1, zero, max_x)
+        y0 = tf.clip_by_value(y0, zero, max_y)
+        y1 = tf.clip_by_value(y1, zero, max_y)
+
+        flat_image_dimensions = height*width
+        pixels_batch = tf.range(batch_size)*flat_image_dimensions
+        flat_output_dimensions = output_height*output_width
+        base = repeat(pixels_batch, flat_output_dimensions)
+        base_y0 = base + y0*width
+        base_y1 = base + y1*width
+        indices_a = base_y0 + x0
+        indices_b = base_y1 + x0
+        indices_c = base_y0 + x1
+        indices_d = base_y1 + x1
+
+        flat_image = tf.reshape(image, shape=(-1, num_channels))
+        flat_image = tf.cast(flat_image, dtype='float32')
+        pixel_values_a = tf.gather(flat_image, indices_a)
+        pixel_values_b = tf.gather(flat_image, indices_b)
+        pixel_values_c = tf.gather(flat_image, indices_c)
+        pixel_values_d = tf.gather(flat_image, indices_d)
+
+        x0 = tf.cast(x0, 'float32')
+        x1 = tf.cast(x1, 'float32')
+        y0 = tf.cast(y0, 'float32')
+        y1 = tf.cast(y1, 'float32')
+
+        area_a = tf.expand_dims(((x1 - x) * (y1 - y)), 1)
+        area_b = tf.expand_dims(((x1 - x) * (y - y0)), 1)
+        area_c = tf.expand_dims(((x - x0) * (y1 - y)), 1)
+        area_d = tf.expand_dims(((x - x0) * (y - y0)), 1)
+        output = tf.add_n([area_a*pixel_values_a,
+                           area_b*pixel_values_b,
+                           area_c*pixel_values_c,
+                           area_d*pixel_values_d])
+        return output
+
+def meshgrid(height, width):
+    with tf.name_scope("meshgrid"):
+        y_linspace = tf.linspace(-1., 1., height)
+        x_linspace = tf.linspace(-1., 1., width)
+        x_coordinates, y_coordinates = tf.meshgrid(x_linspace, y_linspace)    
+        y_coordinates = tf.expand_dims(tf.reshape(y_coordinates, [-1]),0)
+        x_coordinates = tf.expand_dims(tf.reshape(x_coordinates, [-1]),0)
+        indices_grid = tf.concat([x_coordinates, y_coordinates], 0)
+        return indices_grid
+
+def apply_transformation(flows, img, num_channels):
+    with tf.name_scope("apply_transformation"):
+        batch_size = tf.shape(img)[0]
+        height = tf.shape(img)[1]
+        width = tf.shape(img)[2]
+        # num_channels = tf.shape(img)[3]
+        output_size = (height, width)
+        flow_channels = tf.shape(flows)[3]
+
+        flows = tf.reshape(tf.transpose(flows, [0, 3, 1, 2]), [batch_size, flow_channels, height*width])
+
+        indices_grid = meshgrid(height, width)
+
+        transformed_grid = tf.add(flows, indices_grid)
+        x_s = tf.slice(transformed_grid, [0, 0, 0], [-1, 1, -1])
+        y_s = tf.slice(transformed_grid, [0, 1, 0], [-1, 1, -1])
+        x_s_flatten = tf.reshape(x_s, [-1])
+        y_s_flatten = tf.reshape(y_s, [-1])
+
+        transformed_image = interpolate(img, x_s_flatten, y_s_flatten, (height, width))
+
+        transformed_image = tf.reshape(transformed_image, [batch_size, height, width, num_channels])
+        return transformed_image
 
 def batch_norm(x, train_phase, name='bn_layer'):
     #with tf.variable_scope(name) as scope:
@@ -95,88 +201,9 @@ def inference(input_img, input_fp, input_agl, phase_train, conf):
             flow_raw, lcm_input = tf.split(fine_out, [2,2], 3)
 
         flow = tf.nn.tanh(flow_raw)
-        cfw_img = transformation.apply_transformation(flows = flow, img = input_img, num_channels=3)
+        cfw_img = apply_transformation(flows = flow, img = input_img, num_channels=3)
         '''lcm module'''
         lcm_map = lcm_module(lcm_input, lcm_layer, phase_train, name="lcm_module")
         img_pred = apply_lcm(batch_img=cfw_img, light_weight=lcm_map)
 
         return img_pred, flow_raw, lcm_map
-
-def dist_loss(y_pred, y_, method="MAE"):
-    with tf.variable_scope('img_dist_loss'):
-        loss = 0
-        if(method == "L2"):
-            loss = tf.sqrt(tf.reduce_sum(tf.square(y_pred - y_), axis=3, keep_dims = True))
-        elif (method == "MAE"):
-            loss = tf.abs(y_pred - y_)
-        loss = loss[:,img_crop:(-1)*img_crop,img_crop:(-1)*img_crop,:]
-        loss = tf.reduce_sum(loss, axis = [1,2,3])
-        return tf.reduce_mean(loss, axis=0)
-
-def TVloss(inputs):
-    with tf.variable_scope('TVloss'):
-        dinputs_dx = inputs[:, :-1, :, :] - inputs[:, 1:, :, :]
-        dinputs_dy = inputs[:, :, :-1, :] - inputs[:, :, 1:, :]
-        dinputs_dx = tf.pad(dinputs_dx, [[0,0],[0, 1], [0, 0],[0,0]], "CONSTANT")
-        dinputs_dy = tf.pad(dinputs_dy, [[0,0],[0, 0], [0, 1],[0,0]], "CONSTANT")
-        tot_var = tf.add(tf.abs(dinputs_dx), tf.abs(dinputs_dy))
-        tot_var = tf.reduce_sum(tot_var, axis =3, keep_dims=True)
-        return tot_var
-
-def TVlosses(eye_mask, ori_img, flow, lcm_map):
-    with tf.variable_scope('TVlosses'):
-        # eyeball_TVloss
-        # calculate TV (dFlow(p)/dx  + dFlow(p)/dy)
-        TV_flow = TVloss(flow)
-        # calculate the (1-D(p))
-        img_gray = tf.reduce_mean(ori_img, axis = 3, keep_dims=True)
-        ones = tf.ones(shape = tf.shape(img_gray))
-        bright = ones - img_gray
-        # calculate the F_e(p)
-        eye_mask = tf.expand_dims(eye_mask, axis = 3)   
-        weights = tf.multiply(bright,eye_mask)  
-        TV_eye = tf.multiply(weights,TV_flow)
-
-        # eyelid_TVloss
-        lid_mask = ones - eye_mask
-        TV_lid = tf.multiply(lid_mask,TV_flow) 
-        
-        TV_eye = tf.reduce_sum(TV_eye, axis = [1,2,3])
-        TV_lid = tf.reduce_sum(TV_lid, axis = [1,2,3])
-
-        # lcm_map loss
-        dist2cent = center_weight(tf.shape(lcm_map), base=0.005, boundary_penalty=3.0)
-        TV_lcm = dist2cent*TVloss(lcm_map)
-        TV_lcm = tf.reduce_sum(TV_lcm, axis = [1,2,3])
-        
-        return tf.reduce_mean(TV_eye, axis=0), tf.reduce_mean(TV_lid, axis=0), tf.reduce_mean(TV_lcm, axis=0)
-    
-def center_weight(shape, base=0.005, boundary_penalty=3.0):
-    with tf.variable_scope('center_weight'):
-        temp = boundary_penalty - base
-        x = tf.pow(tf.abs(tf.lin_space(-1.0, 1.0,shape[1])),8)
-        y = tf.pow(tf.abs(tf.lin_space(-1.0, 1.0,shape[2])),8)      
-        X, Y = tf.meshgrid(y, x)
-        X = tf.expand_dims(X, axis=2)
-        Y = tf.expand_dims(Y, axis=2)
-        dist2cent = temp*tf.sqrt(tf.reduce_sum(tf.square(tf.concat([X,Y], axis=2)), axis=2)) + base
-        dist2cent = tf.expand_dims(tf.tile(tf.expand_dims(dist2cent, axis=0), [shape[0],1,1]), axis=3)
-        return dist2cent
-
-def lcm_adj(lcm_wgt):
-    dist2cent = center_weight(tf.shape(lcm_wgt), base=0.005, boundary_penalty=3.0)
-    with tf.variable_scope('lcm_adj'):
-        _, loss = tf.split(lcm_wgt, [1,1], 3)
-        loss = tf.reduce_sum(tf.abs(loss)*dist2cent, axis = [1,2,3])
-        return tf.reduce_mean(loss, axis=0)
-    
-def loss(img_pred, img_, eye_mask, input_img, flow, lcm_wgt):
-    with tf.variable_scope('losses'):
-        loss_img = dist_loss(img_pred, img_, method = "L2")
-
-        loss_eyeball, loss_eyelid, loss_lcm= TVlosses(eye_mask, input_img, flow, lcm_wgt)        
-        loss_lcm_adj = lcm_adj(lcm_wgt)
-
-        losses = loss_img + loss_eyeball + loss_eyelid + loss_lcm_adj + loss_lcm 
-        tf.add_to_collection('losses', losses)
-        return tf.add_n(tf.get_collection('losses'), name='total_loss'), loss_img
